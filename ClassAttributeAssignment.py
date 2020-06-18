348235417bf0c46aba7d92e67642ed06ea592f7f
ashlieghdawn <61922862+ashlieghdawn@users.noreply.github.com>
2020-06-17 19:52:56 -0700

    add class assignment

---
+++ ClassAttributeAssignment.py
@@ -0,0 +1,13 @@
+class HospitalStaff:
+    name: 'No Name Provided'
+    email: ' '
+    password: '123abc'
+    officeNumber: 0
+
+class Physician:
+    base_pay: 125.00
+    department: 'Medical'
+
+class AdministrativeAssistant:
+    computer_number: 0
+    security_clearance: True
